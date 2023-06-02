2314bdbcbd16ba4204c60408ecc95a9d7260145d
augustobecker <augustobecker.dev@gmail.com>
2023-06-03 00:09:24 +0200

    fix norminette error.h

--- includes/error.h
+++ includes/error.h
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/03 00:07:34 by acesar-l          #+#    #+#             */
+/*   Updated: 2023/06/03 00:08:46 by acesar-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef ERROR_H
 # define ERROR_H
@@ -6,26 +17,26 @@ typedef enum e_error
 {
 	NOT_AN_ERROR,
 	ERROR_MALLOC,
-    ERROR_FILE,
+	ERROR_FILE,
 	ERROR_ARG,
-    ERROR_MLX,
-    ERROR_TEXTURE,
-    ERROR_MAP,
+	ERROR_MLX,
+	ERROR_TEXTURE,
+	ERROR_MAP,
 	NOT_CONFIGURED_YET
 }	t_error;
 
 typedef enum e_error_arg
 {
 	ARGS_NO_ERROR,
-    INCORRET_ARGS_NBR,
+	INCORRET_ARGS_NBR,
 	INVALID_ARG_EXTENSION
-}   t_error_arg;
+}	t_error_arg;
 
 typedef enum e_error_malloc
 {
 	MALLOC_NO_ERROR,
-    MALLOC_ERROR
-}   t_error_malloc;
+	MALLOC_ERROR
+}	t_error_malloc;
 
 typedef enum e_error_mlx
 {
@@ -37,26 +48,26 @@ typedef enum e_error_mlx
 typedef enum e_error_texture
 {
 	TXTR_NO_ERROR,
-    INVALID_DEFINITION,
+	INVALID_DEFINITION,
 	IS_MISSING_ELEM,
 	MULTIPLE_DEFINITION,
 	INVALID_COLOR
-}   t_error_texture;
+}	t_error_texture;
 
 typedef enum e_error_map
 {
 	MAP_NO_ERROR,
 	INVALID_ELEMENT,
-    MULTIPLE_START_POSITION,
+	MULTIPLE_START_POSITION,
 	MISSING_START_POSITION
-}   t_error_map;
+}	t_error_map;
 
 typedef enum e_error_file
 {
 	FILE_NO_ERROR,
-    FILE_DOESNT_EXIST,
+	FILE_DOESNT_EXIST,
 	FILE_MISS_PERMISSION,
 	FILE_FAILED_TO_OPEN
-}   t_error_file;
+}	t_error_file;
 
 #endif
