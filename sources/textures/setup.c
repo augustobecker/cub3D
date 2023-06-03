df5cae2daa77cd55f20286ebc7042bb3f3aec264
augustobecker <augustobecker.dev@gmail.com>
2023-06-03 22:07:20 +0200

    add textures get data addr

--- sources/textures/setup.c
+++ sources/textures/setup.c
@@ -6,7 +6,7 @@
 /*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/03 20:34:55 by acesar-l          #+#    #+#             */
-/*   Updated: 2023/06/03 21:39:28 by acesar-l         ###   ########.fr       */
+/*   Updated: 2023/06/03 22:06:33 by acesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,18 @@ void	setup_textures(t_data *data)
 	data->so_txtr.img = new_texture(data->mlx_ptr, data->so_txtr.path, data);
 	data->ea_txtr.img = new_texture(data->mlx_ptr, data->ea_txtr.path, data);
 	data->we_txtr.img = new_texture(data->mlx_ptr, data->we_txtr.path, data);
+	data->no_txtr.img.addr = mlx_get_data_addr(data->no_txtr.img.mlx_img, \
+	&data->no_txtr.img.bpp, &data->no_txtr.img.line_len, \
+	&data->no_txtr.img.endian);
+	data->so_txtr.img.addr = mlx_get_data_addr(data->so_txtr.img.mlx_img, \
+	&data->so_txtr.img.bpp, &data->so_txtr.img.line_len, \
+	&data->so_txtr.img.endian);
+	data->ea_txtr.img.addr = mlx_get_data_addr(data->ea_txtr.img.mlx_img, \
+	&data->ea_txtr.img.bpp, &data->ea_txtr.img.line_len, \
+	&data->ea_txtr.img.endian);
+	data->we_txtr.img.addr = mlx_get_data_addr(data->we_txtr.img.mlx_img, \
+	&data->we_txtr.img.bpp, &data->we_txtr.img.line_len, \
+	&data->we_txtr.img.endian);
 }
 
 t_img	new_texture(void *mlx, char *path, t_data *data)
